aef35af09d574b17da5cb37efa83e7651348898e
neutr0n420 <aryan.bramhane@ortigan.in>
2024-06-07 02:17:59 +0530

    contract done frontend init

---
+++ contract/contracts/hello_world/src/lib.rs
@@ -0,0 +1,33 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, log, symbol_short, Env, Symbol};
+const VOTE:Symbol = symbol_short!("VOTE");
+#[contract]
+pub struct VoteContract;
+
+#[contractimpl]
+impl VoteContract {
+    pub fn up_vote(env: Env ) -> i32 {
+        let mut votes:i32 = env.storage().instance().get(&VOTE).unwrap_or(0);
+        votes +=1;
+
+        log!(&env, "New Vote added: {}", votes);
+        env.storage().instance().set(&VOTE, &votes);
+
+        env.storage().instance().extend_ttl(100, 100);
+        votes
+    }
+    pub fn down_vote(env: Env) -> i32  {
+        let mut votes = env.storage().instance().get(&VOTE).unwrap_or(0);
+        log!(&env, "Current Vote: {}", votes);
+
+        votes -=1;
+        log!(&env, "Removed a vote: {}", votes);
+
+        env.storage().instance().set(&VOTE, &votes);
+
+        env.storage().instance().extend_ttl(100, 100);
+        votes
+    }
+}
+
+mod test;
---
+++ contract/contracts/hello_world/src/test.rs
@@ -0,0 +1,17 @@
+#![cfg(test)]
+
+use super::*;
+use crate::{VoteContract, VoteContractClient};
+use soroban_sdk::Env;
+
+#[test]
+fn vote_test() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, VoteContract); 
+    let client = VoteContractClient::new(&env, &contract_id);
+    assert_eq!(client.up_vote(), 1);
+    assert_eq!(client.up_vote(), 2);
+    log!(&env, "Upvote done");
+    assert_eq!(client.down_vote(), 1);
+    assert_eq!(client.down_vote(), 0);
+}
